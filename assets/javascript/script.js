8b48f317cc316875920b95171459a78463c068de
Harrison Stephan <harrisonstephan@Harrisons-iMac.local>
2024-01-09 15:09:06 -0700

    added the API fetching and the links to the css and javascript

---
+++ assets/javascript/script.js
@@ -0,0 +1,33 @@
+let sign = "libra";
+let date = "tomorrow";
+const URL = `https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily?sign=${sign}&day=${date}`;
+
+let sign_search = "libra%20zodiac%20sign";
+let offset = 1;
+const gifURL = `https://api.giphy.com/v1/gifs/search/?api_key=PBjbldrcJfkATLfhbj07XguuikPsv0qv&q=${sign_search}&limit=1&offset=${offset}&rating=g`
+
+fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    });
+
+fetch(gifURL)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
+
+/* Horoscope API
+
+
+
+/*
+
+/* GIPHY API
+
+
+
+
+*/
+
